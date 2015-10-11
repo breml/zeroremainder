827580ceeefbf784b5909db5055c4331275449a8
Lucas Bremgartner <lucas@bremis.ch>
2015-10-11 20:03:56 +0200

    Print count of false calculations.

--- main.go
+++ main.go
@@ -106,20 +106,23 @@ func main() {
 			} else {
 				d = NewDivision(i)
 			}
-			var fastCount, exactCount int
+			var fastCount, exactCount, fastWrong int
 			for j = minDividend; j < maxDividend; j++ {
 				fast := d.IsRestlessDividable(j)
 				exact := (j%i == 0)
-				if fast != exact && false {
-					fmt.Println("Error: ", j, i, d.IsRestlessDividable(j), (j%i == 0), d.GetMn())
-					os.Exit(0)
+				if fast != exact {
+					fastWrong++
+					if false {
+						fmt.Println("Error: ", j, i, d.IsRestlessDividable(j), (j%i == 0), d.GetMn())
+						os.Exit(0)
+					}
 				}
 				fastCount += Btoi(fast)
 				exactCount += Btoi(exact)
 			}
-			if RoundPlus(1.0/float64(exactCount)*float64(fastCount), 4) != 1.0 {
-				fmt.Println("Divisor:", i, "Zeroremainder counts:", fastCount, "Modulo operation counts:", exactCount, "Difference (%):", RoundPlus(100.0/float64(exactCount)*float64(fastCount), 4))
-			}
+			//if RoundPlus(1.0/float64(exactCount)*float64(fastCount), 4) != 1.0 {
+			fmt.Println("Divisor:", i, "Fast wrong:", fastWrong, "Zeroremainder counts:", fastCount, "Modulo operation counts:", exactCount, "Difference (%):", 100.0-RoundPlus(100.0/float64(exactCount)*float64(fastCount), 4))
+			//}
 		}(i)
 	}
 
