a4fd14290135c812303371bcb7238d4f4c046b8b
Lucas Bremgartner <lucas@bremis.ch>
2015-10-15 22:19:26 +0200

    Added support for uint8, uint16 and uint64

--- division.go
+++ division.go
@@ -6,21 +6,23 @@ import (
 )
 
 type Dividable interface {
-	IsRestlessDividable(uint32) bool
-	GetReciprocal() uint32
+	IsRestlessDividable(uint64) bool
+	GetReciprocal() uint64
 }
 
-type Zeroremainder struct {
-	divisor    uint32
-	reciprocal uint32
+// Uint8
+type ZeroremainderUint8 struct {
+	divisor    uint8
+	reciprocal uint8
 }
 
-func NewZeroremainder(divisor uint32) Dividable {
-	zeroremainder := &Zeroremainder{divisor, math.MaxUint32/divisor + 1}
+func NewZeroremainderUint8(divisor uint64) Dividable {
+	zeroremainder := &ZeroremainderUint8{uint8(divisor), math.MaxUint8/uint8(divisor) + 1}
 	return zeroremainder
 }
 
-func (zeroremainder *Zeroremainder) IsRestlessDividable(dividend uint32) bool {
+func (zeroremainder *ZeroremainderUint8) IsRestlessDividable(in_dividend uint64) bool {
+	dividend := uint8(in_dividend)
 	if dividend == 0 {
 		return true
 	}
@@ -30,21 +32,99 @@ func (zeroremainder *Zeroremainder) IsRestlessDividable(dividend uint32) bool {
 	return (dividend * zeroremainder.reciprocal) < zeroremainder.reciprocal
 }
 
-func (zeroremainder *Zeroremainder) GetReciprocal() uint32 {
-	return zeroremainder.reciprocal
+func (zeroremainder *ZeroremainderUint8) GetReciprocal() uint64 {
+	return uint64(zeroremainder.reciprocal)
 }
 
-type DivisionPow2 struct {
+// Uint16
+type ZeroremainderUint16 struct {
+	divisor    uint16
+	reciprocal uint16
+}
+
+func NewZeroremainderUint16(divisor uint64) Dividable {
+	zeroremainder := &ZeroremainderUint16{uint16(divisor), math.MaxUint16/uint16(divisor) + 1}
+	return zeroremainder
+}
+
+func (zeroremainder *ZeroremainderUint16) IsRestlessDividable(in_dividend uint64) bool {
+	dividend := uint16(in_dividend)
+	if dividend == 0 {
+		return true
+	}
+	if dividend < zeroremainder.divisor {
+		return false
+	}
+	return (dividend * zeroremainder.reciprocal) < zeroremainder.reciprocal
+}
+
+func (zeroremainder *ZeroremainderUint16) GetReciprocal() uint64 {
+	return uint64(zeroremainder.reciprocal)
+}
+
+// Uint32
+type ZeroremainderUint32 struct {
 	divisor    uint32
 	reciprocal uint32
 }
 
-func NewDivisionPow2(divisor uint32) Dividable {
+func NewZeroremainderUint32(divisor uint64) Dividable {
+	zeroremainder := &ZeroremainderUint32{uint32(divisor), math.MaxUint32/uint32(divisor) + 1}
+	return zeroremainder
+}
+
+func (zeroremainder *ZeroremainderUint32) IsRestlessDividable(in_dividend uint64) bool {
+	dividend := uint32(in_dividend)
+	if dividend == 0 {
+		return true
+	}
+	if dividend < zeroremainder.divisor {
+		return false
+	}
+	return (dividend * zeroremainder.reciprocal) < zeroremainder.reciprocal
+}
+
+func (zeroremainder *ZeroremainderUint32) GetReciprocal() uint64 {
+	return uint64(zeroremainder.reciprocal)
+}
+
+// Uint64
+type ZeroremainderUint64 struct {
+	divisor    uint64
+	reciprocal uint64
+}
+
+func NewZeroremainderUint64(divisor uint64) Dividable {
+	zeroremainder := &ZeroremainderUint64{divisor, math.MaxUint64/divisor + 1}
+	return zeroremainder
+}
+
+func (zeroremainder *ZeroremainderUint64) IsRestlessDividable(dividend uint64) bool {
+	if dividend == 0 {
+		return true
+	}
+	if dividend < zeroremainder.divisor {
+		return false
+	}
+	return (dividend * zeroremainder.reciprocal) < zeroremainder.reciprocal
+}
+
+func (zeroremainder *ZeroremainderUint64) GetReciprocal() uint64 {
+	return zeroremainder.reciprocal
+}
+
+// Pow2
+type DivisionPow2 struct {
+	divisor    uint64
+	reciprocal uint64
+}
+
+func NewDivisionPow2(divisor uint64) Dividable {
 	divisionPow2 := &DivisionPow2{divisor, divisor - 1}
 	return divisionPow2
 }
 
-func (divisionPow2 *DivisionPow2) IsRestlessDividable(dividend uint32) bool {
+func (divisionPow2 *DivisionPow2) IsRestlessDividable(dividend uint64) bool {
 	if dividend == 0 {
 		return true
 	}
@@ -54,6 +134,6 @@ func (divisionPow2 *DivisionPow2) IsRestlessDividable(dividend uint32) bool {
 	return (dividend & divisionPow2.reciprocal) == 0
 }
 
-func (divisionPow2 *DivisionPow2) GetReciprocal() uint32 {
+func (divisionPow2 *DivisionPow2) GetReciprocal() uint64 {
 	return divisionPow2.reciprocal
 }
--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 )
 
 var (
-	flagDividendStart        = flag.Int("dividendstart", 0, "Start of range of dividends (uint32)")
-	flagDividendEnd          = flag.Int("dividendend", 0xffff, "End of range of dividends (uint32)")
-	flagDivisorStart         = flag.Int("divisorstart", 2, "Start of range of divisors (uint32)")
-	flagDivisorEnd           = flag.Int("divisorend", 1000, "End of range of divisors (uint32)")
+	flagDividendStart        = flag.Int("dividendstart", 0, "Start of range of dividends")
+	flagDividendEnd          = flag.Int("dividendend", 0xffff, "End of range of dividends")
+	flagDivisorStart         = flag.Int("divisorstart", 2, "Start of range of divisors")
+	flagDivisorEnd           = flag.Int("divisorend", 1000, "End of range of divisors")
 	flagMaxProcs             = flag.Int("maxprocs", 0, "Value for GOMAXPROCS (value may be reduced, respecting default GOMAXPROCS and number of CPUs)")
 	flagOutputAllDivisors    = flag.Bool("outputalldivisors", false, "Output result for all divisors")
 	flagOutputAllDifferences = flag.Bool("outputalldifferences", false, "Output every dividend / divisor combination which provides a flase result")
@@ -21,6 +22,7 @@ var (
 	flagIgnorePow2           = flag.Bool("ignorepow2", true, "for power of 2 divisors the solution is trivial, therefore these divisors may be ignored")
 	flagUsePow2              = flag.Bool("usepow2", true, "use bitwise and for power of 2 divisors")
 	flagHelp                 = flag.Bool("help", false, "Print help")
+	flagNumType              = flag.String("numtype", "uint32", "numeric type, used for the calculations (one of: uint8, uint16, uint32, uint64)")
 )
 
 func main() {
@@ -31,43 +33,59 @@ func main() {
 		os.Exit(0)
 	}
 
+	numType := strings.ToLower(*flagNumType)
+	switch numType {
+	case "uint8", "uint16", "uint32", "uint64":
+	default:
+		fmt.Println("numtype must be one of: uint8, uint16, uint32, uint64")
+		os.Exit(1)
+	}
+
 	runtime.GOMAXPROCS(MaxParallelism(*flagMaxProcs))
 
 	var wg sync.WaitGroup
 
-	var minDividend uint32 = uint32(*flagDividendStart)
-	var maxDividend uint32 = uint32(*flagDividendEnd) // fully tested for 0xffffff, may take a long time
+	var minDividend uint64 = uint64(*flagDividendStart)
+	var maxDividend uint64 = uint64(*flagDividendEnd) // fully tested for 0xffffff, may take a long time
 
-	var minDivisor uint32 = uint32(*flagDivisorStart) // min is 2 because negative number and zero don't make sense, and 1 means every packet (no selection)
-	var maxDivisor uint32 = uint32(*flagDivisorEnd)   // tested until 100000
+	var minDivisor uint64 = uint64(*flagDivisorStart) // min is 2 because negative number and zero don't make sense, and 1 means every packet (no selection)
+	var maxDivisor uint64 = uint64(*flagDivisorEnd)   // tested until 100000
 
 	if *flagOutputAll {
 		flag.Set("outputalldivisors", "true")
 		flag.Set("outputalldifferences", "true")
 	}
 
-	var aDivisor uint32
+	var aDivisor uint64
 	for aDivisor = minDivisor; aDivisor <= maxDivisor; aDivisor++ {
-		go func(divisor uint32) {
+		go func(divisor uint64) {
 			wg.Add(1)
 			defer wg.Done()
 
-			var dividend uint32
+			var dividend uint64
 			var d Dividable
 
 			// Check if divisor is a power of 2
 			if divisor&(divisor-1) == 0 {
 				if *flagIgnorePow2 {
 					return
-				} else {
-					if *flagUsePow2 {
-						d = NewDivisionPow2(divisor)
-					} else {
-						d = NewZeroremainder(divisor)
-					}
 				}
-			} else {
-				d = NewZeroremainder(divisor)
+				if *flagUsePow2 {
+					d = NewDivisionPow2(divisor)
+				}
+			}
+
+			if d == nil {
+				switch numType {
+				case "uint8":
+					d = NewZeroremainderUint8(divisor)
+				case "uint16":
+					d = NewZeroremainderUint16(divisor)
+				case "uint32":
+					d = NewZeroremainderUint32(divisor)
+				case "uint64":
+					d = NewZeroremainderUint64(divisor)
+				}
 			}
 
 			var zeroremainderCount, exactCount, zeroremainderWrong int
