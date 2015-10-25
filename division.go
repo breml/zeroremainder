684faca211ef68a5fbf2ff864b4f5fc69af23d3f
Lucas Bremgartner <lucas@bremis.ch>
2015-10-25 14:15:11 +0100

    fixed typo

--- division.go
+++ division.go
@@ -1,6 +1,6 @@
 package main
 
-// type Dividable for Zeroremainder{8,16,31,64} and DivisionPow2
+// type Dividable for Zeroremainder{8,16,32,64} and DivisionPow2
 type Dividable interface {
 	IsRestlessDividable(uint64) bool
 	GetReciprocal() uint64
